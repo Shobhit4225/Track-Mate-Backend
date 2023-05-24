f7e9e12cbcd251b91dfc2c7cb81aac82f0b866eb
Shobhit Singh <shobhit.202354111@vcet.edu.in>
2023-05-24 15:29:43 +0530

    New Route Added

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createUser, getAllUser} from "../controllers/user-controllers";
+import { createUser, getAllUser, getById} from "../controllers/user-controllers";
 
 const router = express.Router();
 
 router.get("/",getAllUser);
 router.post("/createUser",createUser);
+router.get("/:id",getById);
 export default router;
