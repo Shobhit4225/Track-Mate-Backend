662e49bfe2e8aa737be783b93479c8e032c14854
Shobhit Singh <shobhit.202354111@vcet.edu.in>
2023-05-24 22:04:08 +0530

    Modification

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUser, getAllUser, getById} from "../controllers/user-controllers";
+import { createUser, getAllUser, getById , updateLikedPost} from "../controllers/user-controllers";
 import User from '../model/User';
 
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 router.get("/",getAllUser);
 router.post("/createUser",createUser);
 router.get("/:id",getById);
-router.post("/:userId" , updateLikedPost)
+router.post("/update-likes/:userId" , updateLikedPost);
 
 export default router;
